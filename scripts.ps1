51047792e6d0af65f4b8421d40f33b73874e1394
Jon Grey <jon.arnold.grey@gmail.com>
2021-04-11 13:00:05 +0200

    Update sourcing to path from outside current dir

--- scripts.ps1
+++ scripts.ps1
@@ -1,9 +1,7 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-echo "Location $(Get-Location)"
-
-. ./Set-PsEnv.ps1
-. ./Set-IpAddressOctet4.ps1
-. ./Setup-Module.ps1
-. ./Pause.ps1
+. ./powershell-scripts/Set-PsEnv.ps1
+. ./powershell-scripts/Set-IpAddressOctet4.ps1
+. ./powershell-scripts/Setup-Module.ps1
+. ./powershell-scripts/Pause.ps1
